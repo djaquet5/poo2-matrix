28b975998c75c40c828ab0b8921dd1d21a0e6740
Christoph Rueff <christoph.rueff@gmail.com>
2019-02-21 15:02:43 +0100

    Implement Matrix.hpp and includ file in main.cpp

--- Matrix.hpp
+++ Matrix.hpp
@@ -1,4 +1,46 @@
+#include <string>
+
 #ifndef POO2_MATRIX_MATRIX_HPP
 #define POO2_MATRIX_MATRIX_HPP
+
+class Matrix {
+private:
+    size_t n;
+    size_t m;
+    size_t modulo;
+
+    size_t** matrix;
+
+public:
+    // Constructor
+    Matrix(size_t n, size_t m, size_t modulo);
+    // copy
+    Matrix(const Matrix& other);
+
+    // Destructor
+    virtual ~Matrix();
+
+    std::string toString() const;
+
+    // Operations
+    // Returns pointer on result Matrix
+    Matrix* add(const Matrix& other);
+    Matrix* sub(const Matrix& other);
+    Matrix* mult(const Matrix& other);
+
+    // Returns new Matrix (by value)
+    Matrix addAndGetValue(const Matrix& other);
+    Matrix subAndGetValue(const Matrix& other);
+    Matrix multAndGetValue(const Matrix& other);
+
+    // Modifies other Matrix
+    void addOn(Matrix& other);
+    void subOn(Matrix& other);
+    void multOn(Matrix& other);
+
+private:
+    // Matrix operation with Operator op
+    Matrix* operation(Matrix& other, Operator op);
+};
 
 #endif //POO2_MATRIX_MATRIX_HPP
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "Matrix.hpp"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
