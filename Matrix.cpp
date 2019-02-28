fb297e03e52d6ef62d9d688cf5fb1f4447269f45
Christoph Rueff <christoph.rueff@gmail.com>
2019-02-28 15:08:40 +0100

    Add some Matrix methods implementations

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,1 +1,52 @@
 #include "Matrix.hpp"
+
+Matrix::Matrix(size_t n, size_t m, size_t modulo) {
+    if(modulo <= 0) {
+        throw std::invalid_argument("The modulo must be a positive integer\n");
+    }
+
+    auto matrix = new size_t*[n];
+    for(size_t i = 0; i < n; ++i) {
+        matrix[i] = new size_t[m];
+    }
+
+    for(size_t i = 0; i < n; ++i) {
+        for(size_t j = 0; j < m; ++j) {
+            matrix[i][j] = rand() / (RAND_MAX + 1) * n;
+        }
+    }
+
+    this->n = n;
+    this->m = m;
+    this->modulo = modulo;
+    this->matrix = matrix;
+}
+
+Matrix::Matrix(size_t n, size_t m, size_t modulo, size_t **matrix) {
+    if(modulo <= 0) {
+        throw std::invalid_argument("The modulo must be a positive integer\n");
+    }
+
+    for(size_t i = 0; i < n; ++i) {
+        for(size_t j = 0; j < m; ++j) {
+            if(matrix[i][j] < 0 || matrix[i][j] >= modulo) {
+                throw std::invalid_argument("Invalid input table\n");
+            }
+        }
+    }
+
+    this->n = n;
+    this->m = m;
+    this->modulo = modulo;
+    this->matrix = matrix;
+}
+
+Matrix::Matrix(const Matrix &other) {
+
+}
+
+size_t Matrix::at(size_t i, size_t j) const {
+    if(i >= n || j >= m) {
+        throw std::runtime_error("Invalid index\n");
+    }
+}
--- Matrix.hpp
+++ Matrix.hpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <iostream>
 #include "Operator.hpp"
 
 #ifndef MATRIX_HPP
@@ -13,27 +14,45 @@ private:
     size_t** matrix;
 
 public:
-    // Constructor
+    // Constructor (random)
     Matrix(size_t n, size_t m, size_t modulo);
+
+    // Constructor (from data)
+    Matrix(size_t n, size_t m, size_t modulo, size_t** matrix);
+
     // copy
     Matrix(const Matrix& other);
 
+    // Get n
+    size_t getN() const {
+        return n;
+    }
+    // Get m
+    size_t getM() const {
+        return m;
+    }
+    // Get modulo
+    size_t getModulo() const {
+        return modulo;
+    }
+    // get elem
+    size_t at(size_t i, size_t j) const;
     // Destructor
     virtual ~Matrix();
 
-    // change to << operator
-    std::string toString() const;
+    // << operator overload
+    friend std::ostream& operator<<(std::ostream, Matrix& matrix);
 
     // Operations
     // Returns pointer on result Matrix
-    Matrix* add(const Matrix& other);
-    Matrix* sub(const Matrix& other);
-    Matrix* mult(const Matrix& other);
+    Matrix* add(const Matrix& other) const;
+    Matrix* sub(const Matrix& other) const;
+    Matrix* mult(const Matrix& other) const;
 
     // Returns new Matrix (by value)
-    Matrix addAndGetValue(const Matrix& other);
-    Matrix subAndGetValue(const Matrix& other);
-    Matrix multAndGetValue(const Matrix& other);
+    Matrix addAndGetValue(const Matrix& other) const;
+    Matrix subAndGetValue(const Matrix& other) const;
+    Matrix multAndGetValue(const Matrix& other) const;
 
     // Modifies other Matrix
     void addOn(Matrix& other);
@@ -43,6 +62,9 @@ public:
 private:
     // Matrix operation with Operator op
     Matrix* operation(Matrix& other, Operator op);
+
+    // free
+    void free();
 };
 
 #endif // MATRIX_HPP
